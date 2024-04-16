5f9363c90cc1bca2ede8879c69bae2706baf5fd4
vadim-s-v <vadims35v@gmail.com>
2024-04-16 21:21:16 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,55 @@
+public class Main {
+    public static void main(String[] args) {
+        // Задача 1
+        System.out.println("Задача 1 ");
+        var dog = 8;
+        var cat = 3.6;
+        var paper = 763789;
+        System.out.println("dog = "+ dog);
+        System.out.println("cat = "+ cat);
+        System.out.println("paper = "+ paper);
+        // Задача 2
+        System.out.println("Задача 2 ");
+        dog= dog + 4;
+        cat = cat + 4;
+        paper = paper + 4;
+        System.out.println("dog2 = "+ dog);
+        System.out.println("cat2 = "+ cat);
+        System.out.println("paper2 = "+ paper);
+        // Задача 3
+        System.out.println("Задача 3 ");
+        double dog1 = (dog - 3.5);
+        cat = cat - 1.6;
+        paper = paper - 7639;
+        System.out.println("dog3 = "+ dog1);
+        System.out.println("cat3 = "+ cat);
+        System.out.println("paper3 = "+ paper);
+        // Задача 4
+        System.out.println("Задача 4 ");
+        var friend = 19;
+        System.out.println("friend = "+friend);
+        friend = friend + 2;
+        System.out.println("friend + 2 = "+friend);
+        friend =friend / 7;
+        System.out.println("friend / 7 = "+friend);
+        // Задача 5
+        System.out.println("Задача 5 ");
+        // Задача 1
+        System.out.println("Задача 1 ");
+
+        // Задача 1
+        System.out.println("Задача 1 ");
+
+        // Задача 1
+        System.out.println("Задача 1 ");
+        // Задача 1
+        System.out.println("Задача 1 ");
+
+        // Задача 1
+        System.out.println("Задача 1 ");
+        // Задача 1// Задача 1
+
+
+
+    }
+}
